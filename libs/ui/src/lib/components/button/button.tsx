6437708290ac987c0b38536fb88711b1c4c4316b
Rafał Rębilas <rrafal7@gmail.com>
2025-03-04 20:54:00 +0100

    Refactor button component

--- libs/ui/src/lib/components/button/button.tsx
+++ libs/ui/src/lib/components/button/button.tsx
@@ -1,31 +1,28 @@
+'use client';
+
 import React from 'react';
 import styles from './button.module.scss';
 import { makeClasses } from '../../utils/css-classes';
 
 export type ButtonProps = {
-  type: React.ButtonHTMLAttributes<unknown>['type'];
-  children: React.ReactNode;
   size?: 'small' | 'large';
   color: 'primary' | 'secondary' | 'destructive';
-  className?: string;
-  disabled?: boolean;
-  onClick?: () => void;
 };
 
-export function Button({
-  children,
-  type,
+const Button = ({
   className = '',
   size = 'large',
   color,
-  disabled = false,
-  onClick,
-}: ButtonProps) {
+  children,
+  ...props
+}: React.ComponentProps<'button'> & ButtonProps) => {
   const classNames = makeClasses({ styles, moduleClasses: ['button', size, color], className });
 
   return (
-    <button type={type} className={classNames} onClick={onClick} disabled={disabled}>
+    <button className={classNames} {...props}>
       {children}
     </button>
   );
-}
+};
+
+export { Button };
